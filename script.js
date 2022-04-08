cdbeb8739f0c6c1daaf79a81c21d240d1ae4c1d1
Reidzz <danielreid2007@hotmail.co.uk>
2022-04-08 22:46:26 +0100

    Modified if statement and added hidden class in html

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function addDetails() {
     studentScores[i] = document.getElementById("score").value
     i++ // Instead of this maybe try .push() to add values to the arrays
     form.reset()
-    /*const totalResults = studentList.length + studentScores.length
-    if (totalResults >= 14) {
-        submitBtn.removeEventListener("click", addDetails)
+    const totalResults = studentList.length + studentScores.length
+    if (totalResults > 0) {
+        resultsBtn.classList.remove("hidden")
         messageContainer.innerHTML = `${studentList}${studentScores}`
-    }*/
+    }
     console.log(studentList)
     console.log(studentScores)
     console.log(totalResults)
@@ -27,19 +27,3 @@ function addDetails() {
 function displayResults() {
     messageContainer.innerHTML = `${studentList}${studentScores}`
 }
-
-if (totalResults > 0) {
-    resultsBtn.classList.remove("hidden")
-    /*messageContainer.innerHTML = `${studentList}${studentScores}`*/
-}
-
-/*function displayScore() {
-    const nameInput = document.getElementById("student").value
-    const scoreInput = document.getElementById("score").value
-
-    for (i = 0; i < 7; i ++) {
-        studentList[i] = nameInput
-        console.log(nameInput[i])
-        console.log(studentList)
-    }
-}*/
